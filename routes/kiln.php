2e742a73aa826bdc92789463713a464b6fb0d817
Craig Davis <craig@Mini.local>
2012-09-10 14:21:35 -0500

    Update Kiln RSS to be Atom compat

--- routes/kiln.php
+++ routes/kiln.php
@@ -6,26 +6,25 @@
 // Data Source: Kiln RSS Feed
 Use dg\rssPhp;
 
-$app->get('/kiln', $apiAuthenticate(), $setFormat, function () use ($app) {
+$app->get('/kiln', function () use ($app) {
 
     $rss = Feed::loadRss($app->config->kiln_rss);
 
     $res = $app->response();
-    $res['Content-Type'] = 'application/rss+xml';
+    //$res['Content-Type'] = 'application/rss+xml';
  
-    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
-    $rssfeed .= '<rss version="2.0">';
+    $rssfeed  = '<?xml version="1.0" encoding="utf-8"?>';
+    $rssfeed .= '<rss xmlns:a10="http://www.w3.org/2005/Atom" version="2.0">';
     $rssfeed .= '<channel>';
-    $rssfeed .= '<title>' . $item->title . '</title>';
-    $rssfeed .= '<link>' . $item->link .'</link>';
-    $rssfeed .= '<description>' . $item->description . '</description>';
-    $rssfeed .= '<language>en-us</language>';
+    $rssfeed .= '<title>' . $rss->title . '</title>';
+    $rssfeed .= '<link>' . $rss->link .'</link>';
+    $rssfeed .= '<description>' . $rss->description . '</description>';
  
     foreach ($rss->item as $item) {
  
         $rssfeed .= '<item>';
-        $rssfeed .= '<title>' . trim(strip_tags($item->author)) . ': ' .$item->description . '</title>';
-        $rssfeed .= '<description>' . $item->description . '</description>';
+        $rssfeed .= '<title>' . trim(strip_tags($item->author)) . ': ' . strip_tags($item->description) . '</title>';
+        $rssfeed .= '<description>' . strip_tags($item->description) . '</description>';
         $rssfeed .= '<link>' . $item->link . '</link>';
         $rssfeed .= '<pubDate>' . $item->timestamp . '</pubDate>';
         $rssfeed .= '</item>';
